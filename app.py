ee8debb50d5db1afb269c613b0849762c501fb5b
Arnay Garhyan <arnay@Arnays-MacBook-Air.local>
2025-09-08 17:24:09 -0400

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
+
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from db import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Create SQLAlchemy instance **after app**
-db = SQLAlchemy(app)
+db.init_app(app)
 
 # Import models and queries here (after db is created)
-from models import School
-from queries import query_example
+with app.app_context():
+    from models import School
+    from queries import query_example
 
 @app.route("/")
 def index():
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db  
+from db import db
 
 class School(db.Model):
     __tablename__ = "school"
